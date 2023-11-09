10b5ed29e1605a0d8d2bec423b0da1658cb169d9
davemackintosh <me@davemackintosh.co.uk>
2023-11-09 14:46:04 +0000

    Inputs

--- Makefile
+++ Makefile
@@ -3,11 +3,20 @@ APP_NAME:=composeApp
 
 .PHONY: termux-deps
 termux-deps:
+	wget https://github.com/Lzhiyong/termux-ndk/releases/download/android-sdk/android-sdk-aarch64.zip
+	unzip android-sdk-aarch64.zip -d $PREFIX/share
+	@echo "Android SDK installed to $PREFIX/share/android-sdk"
+	direnv allow
+	rm -f android-sdk-aarch64.zip
+
+.PHONY: termux-ubuntu-deps
+termux-ubuntu-deps:
 	${UBUNTU} -- apt install -y android-platform-tools-base android-sdk openjdk-17-jdk sdkmanager gradle
 	${UBUNTU} -- sdkmanager "platform-tools" "platforms;android-33" "build-tools;30.0.2"
 
 .PHONY: build
 build:
-	${UBUNTU} -- /root/www/kotlin/desk/build-in-proot.sh
+	#${UBUNTU} -- /root/www/kotlin/desk/build-in-proot.sh
+	./gradlew assemble
 	cp ./${APP_NAME}/build/outputs/apk/debug/${APP_NAME}-debug.apk ~/storage/shared/Download/
 	# termux-open ~/storage/shared/Download/${APP_NAME}-debug.apk
--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,10 +1,13 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -12,26 +15,31 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
+    var inputExpression by remember { mutableStateOf("") }
+    val focusRequester = remember { FocusRequester() }
+
     MaterialTheme {
-        var greetingText by remember { mutableStateOf("Hello World!") }
-        var showImage by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = {
-                greetingText = "Compose: ${Greeting().greet()}"
-                showImage = !showImage
-            }) {
-                Text(greetingText)
-            }
-            AnimatedVisibility(showImage) {
-                Image(
-                    painterResource("compose-multiplatform.xml"),
-                    null
+        Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(Modifier.background(color = Color.White, shape = RoundedCornerShape(16.dp))) {
+                TextField(
+                        value = inputExpression,
+                        onValueChange = { inputExpression = it },
+                        label = { Text("Enter expression") },
+                        maxLines = 1,
+                        modifier = Modifier.padding(20.dp).focusRequester(focusRequester)
                 )
             }
         }
